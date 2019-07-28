13b5f98803930317c6fec82731e9e6fbc81ce84a
Mahmoud Khaleif <38760489+MahmoudKhaleif93@users.noreply.github.com>
2019-07-28 21:50:51 +0200

    Update Read_from_data_base.py

--- Read_from_data_base.py
+++ Read_from_data_base.py
@@ -1,5 +1,6 @@
 import mysql.connector
-
+#here you put all the info like ip address and name of the database 
+#it is important to make sure that the DB user has a full access so you can use it to retrieve the data. 
 mydb = mysql.connector.connect(
   host="127.0.0.1",
   user="mahmoud",
@@ -9,16 +10,19 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
-#mycursor.execute("SELECT name FROM contidb WHERE ID= 1 ")
+#here you can write my sql queries and specifiy the data which you want. 
+#in my case was a testing data for Vehicles. 
 mycursor.execute("SELECT test_manager , project , software_version FROM test WHERE bench_number=1 ")
-
+#here you fetch the data which you specified. 
 myresult = mycursor.fetchall()
-
+#here you need a loop to output all the letters or the data letters fro the sql tables. 
 for x in myresult:
   print(x)
-
+#another query to another DB accessable with the same User. 
 mycursor.execute("SELECT name FROM contidb WHERE ID= 1 ")
-  
+#here to fetch specified data and output all data via a for loop.   
 myresult1 = mycursor.fetchall()
+#for loop to retrieve the data. 
 for x in myresult1:
   print(x)
+#for GUI you can use a Tkinter Lib in python to have an interface. 
